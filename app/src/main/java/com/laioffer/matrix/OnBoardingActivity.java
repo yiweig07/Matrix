77889426e409a98ac111d94853d2e92dc47a0273
Yiwei Gao <ivygao07@gmail.com>
2020-02-11 00:18:08 -0800

    Firebase authentication

--- app/src/main/java/com/laioffer/matrix/OnBoardingActivity.java
+++ app/src/main/java/com/laioffer/matrix/OnBoardingActivity.java
@@ -1,27 +1,76 @@
 package com.laioffer.matrix;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class OnBoardingActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
+    private FirebaseAuth mAuth;
+    private final static String TAG = OnBoardingActivity.class.getSimpleName();
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_on_boarding);
+        mAuth = FirebaseAuth.getInstance();
 
+        //Add Listener to check sign in status
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+            }
+        };
+
+        //sign in anonymously
+        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "signedInAnonymously", task.getException());
+            }
+        });
+
+        //setup viewpager and tablayout
         viewPager = findViewById(R.id.viewpager);
         OnBoardingPageAdapter onBoardingPageAdapter = new OnBoardingPageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(onBoardingPageAdapter);
 
         TabLayout tabLayout = findViewById(R.id.sliding_tabs);
         tabLayout.setupWithViewPager(viewPager); 
+    }
+
+    //Add authentication listener when activity starts
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    //Remove authentication listener when activity starts
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     // switch viewpage to #page
